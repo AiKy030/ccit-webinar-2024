13e192f0f45564b5b0c5e082d51dcf97fe26bbfc
AiKy030 <151477354+AiKy030@users.noreply.github.com>
2024-04-26 12:32:53 +0800

    Update number-challenge.js

--- number-challenge.js
+++ number-challenge.js
@@ -1,6 +1,38 @@
 /*
-    1 Create a function that will return the sum 3 numbers
+    1 Create a function that will return the sum of 3 numbers
     2 Create a function that will return the difference of the 2 numbers
     3 Create a function that will return the product of the 2 numbers
     4 Create a function that will return the average of the 2 numbers
 */
+
+function so3(num1, num2, num3) 
+{
+    return num1 + num2 + num3; 
+}
+  
+let result0 = so3(4, 5, 3);
+console.log("Sum of 3 Numbers:", result0);
+
+function do2(num1, num2)
+{
+    return num1 - num2;
+}
+
+let result1 = do2(23, 10)
+console.log("Difference of 2 Numbers:", result1)
+
+function po2(num1, num2)
+{
+    return num1 * num2;
+}
+
+let result2 = do2(10, 10)
+console.log("Difference of 2 Numbers:", result2)
+
+function ao2(num1, num2)
+{
+    return num1 / num2;
+}
+
+let result3 = do2(50, 2)
+console.log("Difference of 2 Numbers:", result3)
